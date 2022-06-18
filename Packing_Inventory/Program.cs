404cbbeeda74c130881949e2b5e8e5997af9962f
jb-network <85036487+jb-network@users.noreply.github.com>
2022-06-18 01:06:59 -0500

    Update Program.cs

--- Packing_Inventory/Program.cs
+++ Packing_Inventory/Program.cs
@@ -75,18 +75,22 @@ public class Bow : InventoryItem
 {
     public Bow() : base(1f, 4f) { }
 }
+
 public class Rope : InventoryItem
 {
     public Rope() : base(1f, 1.5f) { }
 }
+
 public class Water : InventoryItem
 {
     public Water() : base(2f, 3f){ }
 }
+
 public class Food : InventoryItem
 {
     public Food() : base(1f, 0.5f) {  }
 }
+
 public class Sword : InventoryItem
 {
     public Sword() : base(5f, 3f) { }
